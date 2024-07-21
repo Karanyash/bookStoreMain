f856e27b75869799225d38cd6cbc4264bd029771
unknown <yashraj.2456@gmail.com>
2024-07-21 23:36:04 +0530

    built to backend server only

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +18,8 @@ app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+// const URI = process.env.MongoDBURI;
+const URI = process.env.MONGO_ONLINE_URL;
 
 // connect to mongoDB
 try {
@@ -30,6 +35,21 @@ try {
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+
+// Deployment
+if (process.env.NODE_ENV === "production") {
+    const dirPath = path.resolve();
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(dirPath, "Frontend", "dist", "index.html"));
+    });
+}
+
+app.get("/", (req, res) => {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+    res.sendFile(path.resolve(__dirname, "../Frontend/dist/index.html"));
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
